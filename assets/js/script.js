49c5c8b8b8d7449001f217a251b0b8eec49a1722
Vilson Riegel <vilsonriegel88@hotmail.com>
2023-03-24 20:56:51 -0300

    adicionado-javascript-em-algumas-partes-do-site

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+// Ativar Links do Menu
+
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+  
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+
+function eventosPergunta(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPergunta);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = window.matchMedia('(min-width: 937px)').matches;
+  if (media){
+    galeriaContainer.prepend(img);
+  }
+}
+
+function enventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(enventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();  
+}
